f18bd7256430791f7750abe88ca19cc629a64017
davi.bento <davi.bento@itaesbra.com.br>
2022-10-05 14:32:59 -0300

    Adicionar arquivos de projeto.

---
+++ AppPingAndroid/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Xamarin.Forms;
+
+namespace AppPingAndroid
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage())
+            {
+                BarTextColor = Color.Black,
+            };
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ AppPingAndroid/Empresas.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppPingAndroid
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnDiadema_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnIgarassu_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void BtnSair_Clicked(object sender, EventArgs e)
+        {
+            var resultado = await DisplayAlert("Ping", "Deseja mesmo sair?", "Sim", "Não");
+            if (resultado)
+            {
+                System.Environment.Exit(0);
+            }
+        }
+    }
+}
